c67efc91207353cf8c44fdef5a3e3236dcf5f509
tusbea <tusbea@gmail.com>
2015-04-10 13:03:41 +0900

    model validate and error manage

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,14 +1,17 @@
 class LoginController < ApplicationController
   def index
+    @user = User.new
   end
 
   def create
     @user = User.new(params.require(:user).permit(:username, :password))
 
     if params[:login]
-
+      render 'index'
     elsif params[:add]
-
+      if !@user.save
+        render 'index'
+      end
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true,
+                       length: { 
+    minimum: 5,
+    maximum: 20,
+    wrong_length: "The user name should be 5~20 characters long. Please try again." 
+  }
+  
+  validates :password, presence: true,
+                       length: { 
+    minimum: 8,
+    maximum: 20,
+    wrong_length: "The password should be 8~20 characters long. Please try again."
+  }
 end
