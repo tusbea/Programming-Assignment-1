372ea13af53a2b9e3abe68e8ba6578de72ff7712
tusbea <tusbea@gmail.com>
2015-04-10 13:29:16 +0900

    welcome page added

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,17 +1,24 @@
 class LoginController < ApplicationController
   def index
     @user = User.new
+    @users = User.all
   end
 
   def create
     @user = User.new(params.require(:user).permit(:username, :password))
 
     if params[:login]
-      render 'index'
+      render 'welcome'
     elsif params[:add]
       if !@user.save
         render 'index'
+      else
+        render 'welcome'
       end
     end
   end
+
+  def welcome
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ActiveRecord::Base
-  validates :username, presence: true,
-                       length: { 
+  validates :username, length: { 
     minimum: 5,
     maximum: 20,
-    wrong_length: "The user name should be 5~20 characters long. Please try again." 
+    too_long: "The user name should be 5~20 characters long. Please try again.", 
+    too_short: "The user name should be 5~20 characters long. Please try again." 
   }
   
-  validates :password, presence: true,
-                       length: { 
+  validates :password, length: { 
     minimum: 8,
     maximum: 20,
-    wrong_length: "The password should be 8~20 characters long. Please try again."
+    too_long: "The password should be 8~20 characters long. Please try again.",
+    too_short: "The password should be 8~20 characters long. Please try again."
   }
 end
